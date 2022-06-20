20bd8dd15408908135db76f90a8237c530433531
Andeo1812 <alexanforall@gmail.com>
2022-06-20 23:28:56 +0300

    add demo

--- Askme/urls.py
+++ Askme/urls.py
@@ -36,4 +36,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
